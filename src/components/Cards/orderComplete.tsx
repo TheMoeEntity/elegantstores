c1ded2d90bead50bbb8552805b758c3330eabe61
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-04-01 22:59:15 +0100

    "Added motion components, updated layout and added OrderComplete component for different steps in the cart process"
    This commit includes the following changes:
    
    * Added motion components from the framer-motion library for animating the entrance of different sections of the cart page.
    * Updated the layout of the cart page to include different steps in the cart process, such as Shopping cart, Checkout details, and Order complete.
    * Added the OrderComplete component, which is displayed when the user reaches the Order complete step.

---
+++ src/components/Cards/orderComplete.tsx
@@ -0,0 +1,28 @@
+import styles from './cards.module.css'
+
+const OrderComplete = () => {
+    return (
+        <div className={`${styles.receivedOrderContainer} text-center py-5`}>
+            <p className="font-weight-bold h3 mb-0">
+                Your Order Has Been Received! 🥳
+            </p>
+            <i className="fa-solid fa-check-mark" />
+            <p className="mt-2">
+                We've recieved your order and will be shipping it to you as soon as
+                possible.
+            </p>
+
+            <button
+                className="bg-black text-white rounded-md mt-8"
+                style={{ padding: "10px", fontSize: "small" }}
+                onClick={() => {
+                    window.location.href = "/myaccount";
+                }}
+            >
+                Purchase History
+            </button>
+        </div>
+    );
+}
+
+export default OrderComplete
