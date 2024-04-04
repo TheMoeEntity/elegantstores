1f5a0e63a874d5bd12986a7a0068c05430187a35
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-04-04 11:27:51 +0100

    "Updated URL for fetching products in Shop component"
    This commit updates the URL used for fetching products in the Shop component from "<https://api.escuelajs.co/api/v1/categories/2/products>" to "<https://api.escuelajs.co/api/v1/categories/4/products>".

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -4,7 +4,7 @@ const ShopPage = dynamic(() => import("../../components/Shop"));
 
 const Shop = async () => {
     const products = await Helpers.getProducts(
-        "https://api.escuelajs.co/api/v1/categories/2/products" ?? []
+        "https://api.escuelajs.co/api/v1/categories/4/products" ?? []
     );
     return (
         <main className="max-w-7xl mx-auto">
